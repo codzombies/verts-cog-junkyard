aca0c59b5663da7be49ce0171d1b0bed784c6526
EternalllZM <43221983+EternalllZM@users.noreply.github.com>
2023-08-07 17:01:56 -0500

    Update __init__.py
    red 3.5

--- warntransfer/__init__.py
+++ warntransfer/__init__.py
@@ -9,9 +9,6 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot):
+async def setup(bot):
     cog = WarnTransfer(bot)
-    if discord.__version__ > "1.7.3":
-        await bot.add_cog(cog)
-    else:
-        bot.add_cog(cog)
+    await bot.add_cog(cog)
